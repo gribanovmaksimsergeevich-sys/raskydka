fc4ee64b7da0aa1a5edeea5794198a154a7cbb39
gribanovmaksimsergeevich-sys <gribanovmaksimsergeevich@gmail.com>
2026-08-03 15:29:16 +0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,25 @@
+// Service Worker для PWA «ТТК Адреска»
+const CACHE_NAME = 'ttk-adreska-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
